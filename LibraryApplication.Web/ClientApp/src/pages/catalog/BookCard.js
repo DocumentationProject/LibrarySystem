041785d802313e42672f4d8bfca090c9339e81bd
Nazar <milkysdave@gmail.com>
2023-06-14 22:50:25 +0300

    Improve card view

--- LibraryApplication.Web/ClientApp/src/pages/catalog/BookCard.js
+++ LibraryApplication.Web/ClientApp/src/pages/catalog/BookCard.js
@@ -1,26 +1,45 @@
 import React, {useState} from 'react';
-import {Card} from "antd";
+import {Button, Card, notification, Statistic} from "antd";
 import {BookOutlined, DeleteOutlined, EditOutlined} from "@ant-design/icons";
 import {useUser} from "../../hooks/useUser";
 import BorrowBookModal from "./BorrowBookModal";
+import {API} from "../../configs/axios.config";
+import Paragraph from "antd/es/typography/Paragraph";
 
-const BookCard = ({book}) => {
+const BookCard = ({book, setBooks}) => {
     const { user } = useUser();
 
     const [showBorrowBookModal, setShowBorrowBookModal] = useState(false)
 
-    const cardActions = [<BookOutlined key="borrow" onClick={() => setShowBorrowBookModal(true)} />,]
+    const handleDeleteBook = async () => {
+        try {
+            await API.delete(`/api/Book/${book.id}/delete`)
+            notification.success({message: 'Book deleted!'})
+            setBooks(prevBooks => prevBooks.filter(b => b.id !== book.id))
+        } catch (e) {
+            console.log(e)
+        }
+    }
+
+    const cardActions = [<Button type='text' onClick={() => setShowBorrowBookModal(true)} icon={<BookOutlined key="borrow"  />} disabled={!book.isAvailable}/>]
     if (user.isAdmin) {
-        cardActions.unshift(<DeleteOutlined key="delete" />)
-        cardActions.unshift(<EditOutlined key="edit" />)
+        cardActions.unshift(<Button type='text' onClick={handleDeleteBook} icon={<DeleteOutlined key="delete"  />}/>)
+        cardActions.unshift(<Button type='text' icon={<EditOutlined key="edit" />}/>)
     }
 
     return (
         <Card
+            title={book.name}
             actions={cardActions}
         >
             {showBorrowBookModal && <BorrowBookModal book={book} setShowModal={setShowBorrowBookModal}/>}
-            <Card.Meta title={book.name}/>
+            <Paragraph>Author: {book.authorId}</Paragraph>
+            <Paragraph>Genre: {book.genreId}</Paragraph>
+            <Statistic
+                title="Rent price"
+                value={book.rentPrice}
+                prefix="$"
+            />
         </Card>
     );
 };
